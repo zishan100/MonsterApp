588dee86aaef8473297bb5c88709806147e29a7e
zishan100 <Zishanansari111@gmail.com>
2020-07-23 20:19:23 +0530

    Updates

---
+++ precache-manifest.67f5f2255cb5be8032b90a5989b332ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "38472800d3b276f0d365",
+    "url": "/MonsterApp/static/css/main.d7aa8934.chunk.css"
+  },
+  {
+    "revision": "38472800d3b276f0d365",
+    "url": "/MonsterApp/static/js/main.38472800.chunk.js"
+  },
+  {
+    "revision": "fadca08370a61f01af57",
+    "url": "/MonsterApp/static/js/1.fadca083.chunk.js"
+  },
+  {
+    "revision": "938b56e9d0085752d1d4",
+    "url": "/MonsterApp/static/js/runtime~main.938b56e9.js"
+  },
+  {
+    "revision": "53c95ab23e18f99de555df26c587f284",
+    "url": "/MonsterApp/index.html"
+  }
+];
